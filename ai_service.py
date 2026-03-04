dad2723d25c262d1407331e6a5d204782591d814
arkaadiana <arkaadiana.lit@gmail.com>
2026-03-04 12:56:04 +0800

    Update api calling

--- ai_service.py
+++ ai_service.py
@@ -4,6 +4,10 @@ import subprocess
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
+
+from dotenv import load_dotenv
+load_dotenv()
+
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel('gemini-1.5-flash')
 
