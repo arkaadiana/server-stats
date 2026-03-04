f98d95c564798a630ced267e76bb1a22286db703
arkaadiana <arkaadiana.lit@gmail.com>
2026-03-04 12:59:41 +0800

    Update model gemini

--- ai_service.py
+++ ai_service.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 COMMAND_MAP = {
     "REBOOT_SERVER": ["sudo", "/usr/sbin/reboot"],
